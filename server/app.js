ad8dd02c116a5d035f7dcc0ae82d2e2a18823e33
Patrick Stens <pstens@mail.uni-paderborn.de>
2018-01-28 01:33:43 +0100

    Add more test stuff

--- server/app.js
+++ server/app.js
@@ -1,13 +1,20 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
+const messages = []
+app.use(bodyParser.json())
 
 app.get(['/random', '/r'], function (req, res) {
-  const random = Math.random() * 100
-  console.log(`Request from ${req.ip} generated: ${random}`)
-  console.log(req.hostname)
-  console.log(req.get('user-agent'))
-  res.status(200).send(`${random}`)
+  const item = messages[Math.floor((Math.random() * messages.length))]
+  res.status(200).send(item)
+})
+
+app.post('/random', function (req, res) {
+  const message = req.body.message
+  messages.push(message)
+  console.info(`Post-Request from ${req.ip} with body: ${message}`)
+  res.sendStatus(201)
 })
 
 const port = 3000
